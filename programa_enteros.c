a90a385f4c1f17057e1aab39e9e5c8bd338e3e98
elimer <42146814+elimer@users.noreply.github.com>
2018-08-16 22:56:36 -0500

    Add files via upload

---
+++ programa_enteros.c
@@ -0,0 +1,22 @@
+/*Creado el 15 de Agosto del 2018 por Elizabeth Mercado*/
+
+#include<stdio.h>
+
+int main()
+
+{
+int a,b,c,d,e;
+printf("Escriba cuatro números enteros \n");
+scanf($i %i %i %i , &a, &b, &c, &d);
+
+e=(a+b)*c/d;
+printf("El resultado de la operacion e=(a+b)*c/d es %i \n", e);
+e=((a+b)*c)/d;
+printf("El resultado de la operación e=((a+b)*c)/d es %i \n" e);
+e=(a+b)*c/d;
+printf("El resultado de la operación e=(a+b)*c/d es %i \n" e);
+e=a+(b*c)/d;
+printf("El resultado de la operación e=a+(b*c)/d es %i \n" e);
+return0
+
+}
