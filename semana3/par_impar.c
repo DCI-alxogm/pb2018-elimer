8486a67339ee849e184f9b5be3ed8c2927f161dd
elimer <liz.eli_@hotmail.com>
2018-08-22 09:58:41 -0500

    Raelice los ejemplos

---
+++ semana3/par_impar.c
@@ -0,0 +1,11 @@
+/*Creado el 22 e Agosto del 2018 por Elizabeth Mercado*/
+
+#include<stdio.h>
+
+int main()
+{
+	int numero1, numero2;
+	printf("Introduzca dos numeros enteros: ");
+	scanf("%i %i", &numero1, &numero2);
+
+	if(numero1==numero2)
