09cc784a93092d426e3abab7fd42d6c24a33bde1
elimer <liz.eli_@hotmail.com>
2018-08-22 09:09:30 -0500

    Subi dos archivos mas

--- semana3/programa_x.c
+++ semana3/programa_x.c
@@ -10,8 +10,11 @@ int main()
 	printf("Escriba una coordenada para x \n");
 	scanf("%f",&x,&y);
 	y=exp(-1*x);
+	printf("Escriba una coordenada para x \n")
 	x=(x*(pi/180));
+	printf("Escriba una coordenada para x \n")
 	y=(cos(x) + 2*tan(x/2));
+	printf("Escriba una coordenada para x \n")
 	y=(log(x)+3*(pow(x,2)));
 	printf("\nLos resultados en y son: %f \n",x);
        return 0;
