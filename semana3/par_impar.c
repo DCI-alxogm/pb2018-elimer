8d88cf9185cfc1b57a443e9da831442523e838a3
Mercado Ruiz Elizabeth <liz.eli_@hotmail.com>
2018-08-24 10:46:12 -0500

    Actualice dos archivos

--- semana3/par_impar.c
+++ semana3/par_impar.c
@@ -8,4 +8,37 @@ int main()
 	printf("Introduzca dos numeros enteros: ");
 	scanf("%i %i", &numero1, &numero2);
 
-	if(numero1==numero2)
+	if(numero1==numero2);
+	printf("Introduzca dos numeros enteros: \n");
+
+scanf("%i %i", &numero1, &numero2);
+
+
+
+if(numero1==numero2)
+
+{
+
+printf("Resultado; %d",numero1,numero2);
+
+}
+
+else if (numero1> numero2)
+
+{
+
+printf("Resultado: %d > %d", numero1,numero2);
+
+}
+
+else
+
+{
+
+printf("Result: %d < %d",numero1, numero2);
+
+}
+
+return 0;
+
+}
