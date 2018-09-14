230351ef1b65c5a31bb7fea6938f8cf0f1fb1bb3
elimer <liz.eli_@hotmail.com>
2018-09-14 18:12:51 -0500

    Actualizado

---
+++ semana2/programa_enteros.c
@@ -0,0 +1,22 @@
+/*Creado el 15 de Agosto de 2018 por Elizabeth Mercado*/
+
+#include<stdio.h>
+
+int main()
+
+{
+int a,b,c,d;
+printf("Escriba cuatro números enteros \n");
+scanf("%i %i %i %i, &a, &b, &c, &d,);
+
+e=(a+b)*c/d;
+printf("El resultado de la operación e=(a+b)*c/d es %i\n",e);
+e=((a+b)*c)/d;
+printf("El resultado de la operación e=((a+b)*c)/d es %i\n",e);
+e=(a+b)*c/d;
+printf("El resultado de la operación e=(a+b)*c/d es %i\n",e);
+e=a+((b*c)/d;
+printf("El resultado de la operación e=a+(b*c)/d es %i\n"e);
+return0
+
+}
