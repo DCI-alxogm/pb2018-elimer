45dd36d2e92789321ecd2f612bb36c5a0a49adfe
elimer <liz.eli_@hotmail.com>
2018-09-19 10:01:11 -0500

    Actualizado

--- semana6/lectura2.c
+++ semana6/lectura2.c
@@ -3,7 +3,7 @@
 
 int main(){
 	FILE *archivo;
-	int n=10;
+	int n=3;
 	int edad[n],sexo[n],semestre[n],calificacion[n];
 	int m=0,h=0,sex=0;
 	int s[9];
@@ -12,7 +12,7 @@ int main(){
 
 	archivo=fopen("info.txt","w");
 	
-	for(i=1;i<=10;i++){
+	for(i=1;i<=n;i++){
 	printf("Escribe 1 si eres mujer y 0 si eres hombre\n");
 	printf("Escribe la informacion del estudiante %i: edad,sexo,semestre,calificación\n",i);
 	scanf("%i %i %i %i",&edad[i],&sexo[i],&semestre[i],&calificacion[i]);
@@ -27,18 +27,20 @@ int main(){
 		h++;
 	
 
-	for(j=1;j<=9;j++)
+	for(j=0;j<9;j++){
 		if(semestre[i]==j) s[j]+=1;
-	
+		else 
+			s[j]++;
+		 
+	}
 
 
 		prom=(prom+calificacion[i]);
 	}
 prom=prom/n;
 printf("Mujeres=%i,hombres=%i\n",m,h);
-for(j=1;j<=9;j++){
-printf("Estudiantes de semestre: 1=%i, 2=%i, 3=%i, 4=%i, 5=%i, 6=%i, 7=%i, 8=%i\n",j);
-}
+
+printf("Estudiantes de semestre: 1=%i, 2=%i, 3=%i, 4=%i, 5=%i, 6=%i, 7=%i, 8=%i 9=%i\n",j);
 printf("El promedio de alumnos= %f\n",prom);
 
 return 0;
