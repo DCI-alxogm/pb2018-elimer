5041a5e58f613603cb3fd6feca7bb40986216b2c
elimer <liz.eli_@hotmail.com>
2018-10-24 09:22:16 -0500

    actualizado

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -3,9 +3,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void temperatura//(x,y,Tx,Ty)
 int main(){
-	float e: ;
-	int N,n;
-	float Txy[]
+	float e:0.001 ;
+	int N=n*n,n=0;
+	float Txy[N],Tx[N],y[N],x[N];
+	double *ptr= (double*)malloc(N*sizeof(double));
+	FILES archivo1*;
+	//FILES archivo2*;
 
+//leer del archivo la temperatura de los bordes,numero de puntos a usar en la aproximación,
+archivo1=fopen("datos.txt","r")
+if(i=0;i<N;i++){
+Txy[i]=(Tx[i]+1,y[i]+Tx[i]-1,y[i]+Tx[i],y[i]+1+Tx[i],y[i]-1)/4
+
+h=(T[i]-T[i-1])/4
+}
+
+if(y[i]=0){
+Txy[i]=(Tx[i]+1,y[i]+Tx[i]-1,y[i]+Tx[i],y[i]+1+Tx[i],y[i]-1)/2
+}
+fclose(archivo1);
+(Txy[i]-Txy[i-1])/Txyi-1]<e;
+free(ptr);
+return 0;
+}
