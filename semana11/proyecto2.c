99f23f39996eade7bdeb1ee7ca552d409b874961
elimer <liz.eli_@hotmail.com>
2018-10-26 13:14:59 -0500

    Avance

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -3,47 +3,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//int main(){
-void funcion();
-void datos(float left,float right,float up, float down, int n,float T**);
 
-//se crea otra funcion donde se van a reañizar todas las operaciones del programa
+
+void funcion();
+//void datos(float left,float right,float up, float down, int n);
+
+//se crea otra funcion donde se van a realizar todas las operaciones del programa
 void funcion(){
-	float e,w;
-	int N,n=0,i,j;
+	float e;
+	int N,n=0,i,j,w;
 	float left,right,up,down;
-	float x[N],y[N],T[i][j];
-	FILE first*, results*;
-	N=n*n;	
+	
+	FILE *first;
+	FILE *results;
+	
 
 //leer del archivo la temperatura de los bordes,numero de puntos a usar en la aproximación,
 first=fopen("datos.txt","r");
-fscanf(" %f %f %f %f %f %i ",&left,&right,&up,&down,&e,&n);
+fscanf(first,"%f %f %f %f %f %i",&left,&right,&up,&down,&e,&n);
+
 fclose(first);
 
-double *T= (double*)malloc(n*sizeof(double));
+
+
+double **T= (double*)malloc(n*sizeof(double*));
 for(i=0;i<n;i++){
-T[i]=(double**)malloc(n*sizeof(double));
+T[i]=(double*)malloc(n*sizeof(double));
 }
 
-datos(left,right,up,down,n,T);
-
-
-results=fopen("resultados.txt","w");
-for(i=0;i<N;i++){
-	while(w<e){
-	T[i][j]=(((T[i+1],[j]+T[i-1],[j]+T[i],y[i+1]+T[i],[j-1]))/4);
-	w=((T[i][j]-T[i-1][j-1])/T[i-1][j-1]);
-	fprintf("%f %f %f",x[i],y[j],T[i][j]);//
-}
-}
-fclose(results);
-free(T);
-//return 0;
-}
-//Segunda funcion donde se meteran y leeran los datos iniciales dados en el archivo  de texto y sirve para determinar la temperatura solo en las orillas de la placa
-void datos(float left,float right,float up, float down, int n,float T**);{
-int i,j;
 for(i=0;i<n;i++){
 		for(j=0;j<n;j++){
 		if(i=0)
@@ -58,4 +45,44 @@ for(i=0;i<n;i++){
 		T[i][j]=0;
 	}
 }
+
+
+//datos(left,right,up,down,n,T);
+w=5;
+N=n*n;	
+results=fopen("resultados.txt","w");
+for(i=1;i<=N;i++)
+{
+		for(j=1;j<=N;j++)
+		{ //se ponen dos ciclos anidados ya que son dos vectores.
+		while(w<e){ //condicion para que se pueda realzar el programa
+		T[i][j]=((T[i+1][j]+T[i-1][j]+T[i][j+1]+T[i][j-1])/4);
+		printf("Resultados\n",T[i][j]);
+		
+fprintf(results,"%i %i %lf\n",i,j,T[i][j]);//
+}
+}
+}
+fclose(results);
+free(T);
+}
+
+//Segunda funcion donde se meteran y leeran los datos iniciales dados en el archivo  de texto y sirve para determinar la temperatura solo en las orillas de la placa
+//void datos(float left,float right,float up, float down, int n){
+//int i,j;
+//float T[N][N];
+/*for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+		if(i=0)
+		T[i][j]=left;
+		else if(i=n)
+		T[i][j]=right;
+		else if(j=0)
+		T[i][j]=up;
+		else if(j=n)
+		T[i][j]=down;
+		else
+		T[i][j]=0;
+	}
+}*/
 
